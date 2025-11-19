651748e978639e44ddee2b98f8b41a2cb3e4a7f9
xy233-hub <825717abcABC>
2025-11-19 10:35:43 +0800

    feat(upgrade-a-product-fe): 商品管理升级前端（多商品发布表单+分类选择+库存显示+搜索组件+多图上传）

--- frontend/shop-front/src/App.vue
+++ frontend/shop-front/src/App.vue
@@ -1,3 +1,31 @@
 <template>
-  <router-view />
+  <div id="app">
+    <router-view />
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+}
+
+.el-main {
+  padding: 20px;
+}
+</style>
