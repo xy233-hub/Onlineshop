42a169919bf61961060f3242e3e0107d77840a77
xy233-hub <825717abcABC>
2026-03-31 15:37:21 +0800

    版本标识

--- frontend/shop-front/src/views/Home.vue
+++ frontend/shop-front/src/views/Home.vue
@@ -4,7 +4,7 @@
     <header class="header">
       <div class="header-inner">
         <div class="nav-brand">
-          <h1>光盘行动——C版</h1>
+          <h1>光盘行动-C版</h1>
         </div>
 
         <div class="nav-links">
